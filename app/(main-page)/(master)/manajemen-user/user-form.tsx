c08c451f32b957f64ae65bf0ec1352f1e3dadfbf
rizal15D <muhammadrizal15122001@gmail.com>
2024-06-07 10:17:56 +0700

    menu pilih prodi

--- app/(main-page)/(master)/manajemen-user/user-form.tsx
+++ app/(main-page)/(master)/manajemen-user/user-form.tsx
@@ -61,7 +61,6 @@ export default function UserForm({
               className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
             />
           </div>
-
           <div className="mb-4.5">
             <label className="mb-2.5 block text-black dark:text-white">
               Email
@@ -73,7 +72,6 @@ export default function UserForm({
               className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
             />
           </div>
-
           <div className="mb-4.5">
             <label className="mb-3 block text-black dark:text-white">
               Jabatan
@@ -115,48 +113,45 @@ export default function UserForm({
               </span>
             </div>
           </div>
-
-          {isProdi ? (
-            <div className="mb-4.5">
-              <label className="mb-3 block text-black dark:text-white">
-                Prodi
-              </label>
-              <div className="relative z-20 bg-white dark:bg-form-input">
-                <select
-                  name="prodi_id"
-                  disabled={!isProdi}
-                  className="relative z-20 disabled:cursor-not-allowed disabled:bg-disabled w-full appearance-none rounded border border-stroke bg-transparent py-3 px-5 outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input"
+          {/* {isProdi ? ( */}
+          <div className="mb-4.5">
+            <label className="mb-3 block text-black dark:text-white">
+              Prodi
+            </label>
+            <div className="relative z-20 bg-white dark:bg-form-input">
+              <select
+                name="prodi_id"
+                // disabled={!isProdi}
+                className="relative z-20 disabled:cursor-not-allowed disabled:bg-disabled w-full appearance-none rounded border border-stroke bg-transparent py-3 px-5 outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input"
+              >
+                {prodi.map((prodi: any) => (
+                  <option key={prodi.id} value={prodi.id}>
+                    {prodi.name}
+                  </option>
+                ))}
+              </select>
+              <span className="absolute top-1/2 right-4 z-10 -translate-y-1/2">
+                <svg
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
                 >
-                  {prodi.map((prodi: any) => (
-                    <option key={prodi.id} value={prodi.id}>
-                      {prodi.name}
-                    </option>
-                  ))}
-                </select>
-                <span className="absolute top-1/2 right-4 z-10 -translate-y-1/2">
-                  <svg
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <g opacity="0.8">
-                      <path
-                        fillRule="evenodd"
-                        clipRule="evenodd"
-                        d="M5.29289 8.29289C5.68342 7.90237 6.31658 7.90237 6.70711 8.29289L12 13.5858L17.2929 8.29289C17.6834 7.90237 18.3166 7.90237 18.7071 8.29289C19.0976 8.68342 19.0976 9.31658 18.7071 9.70711L12.7071 15.7071C12.3166 16.0976 11.6834 16.0976 11.2929 15.7071L5.29289 9.70711C4.90237 9.31658 4.90237 8.68342 5.29289 8.29289Z"
-                        fill="#637381"
-                      ></path>
-                    </g>
-                  </svg>
-                </span>
-              </div>
+                  <g opacity="0.8">
+                    <path
+                      fillRule="evenodd"
+                      clipRule="evenodd"
+                      d="M5.29289 8.29289C5.68342 7.90237 6.31658 7.90237 6.70711 8.29289L12 13.5858L17.2929 8.29289C17.6834 7.90237 18.3166 7.90237 18.7071 8.29289C19.0976 8.68342 19.0976 9.31658 18.7071 9.70711L12.7071 15.7071C12.3166 16.0976 11.6834 16.0976 11.2929 15.7071L5.29289 9.70711C4.90237 9.31658 4.90237 8.68342 5.29289 8.29289Z"
+                      fill="#637381"
+                    ></path>
+                  </g>
+                </svg>
+              </span>
             </div>
-          ) : (
-            <input type="hidden" name="prodi_id" value="1" />
-          )}
-
+          </div>
+          {/* ) : (
+          <input type="hidden" name="prodi_id" value="1" />)} */}
           <div className="mb-4.5">
             <label className="mb-3 block text-black dark:text-white">
               Fakultas
@@ -192,7 +187,6 @@ export default function UserForm({
               </span>
             </div>
           </div>
-
           <button className="flex w-full justify-center rounded bg-primary p-3 font-medium text-gray">
             {isLoading ? (
               <div className="h-6 w-6 animate-spin rounded-full border-4 border-solid border-white border-t-transparent"></div>
